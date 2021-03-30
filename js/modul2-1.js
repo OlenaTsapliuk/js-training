a498030ec09f48e6aeee743fb2a9b5b3f3ced350
OlenaTsapliuk <74963920+OlenaTsapliuk@users.noreply.github.com>
2021-03-31 00:50:41 +0300

    Modul2
    задачи до21

--- js/modul2-1.js
+++ js/modul2-1.js
@@ -90,10 +90,82 @@
 // console.log(slugify('Маcсивы для новичков'));
 
 // 13
-const fruits = ['яблоко', 'слива', 'груша', 'апельсин', 'банан'];
-// Пиши код ниже этой строки
-const firstTwoEls = fruits.slice(0, 1);
-const nonExtremeEls = fruits.slice(1, 3);
-const lastThreeEls = fruits.slice(-3);
+// const fruits = ['яблоко', 'слива', 'груша', 'апельсин', 'банан'];
+// // Пиши код ниже этой строки
+// const firstTwoEls = fruits.slice(0, 2);
+// const nonExtremeEls = fruits.slice(1, 4);
+// const lastThreeEls = fruits.slice(-3);
 
-console.log(firstTwoEls);
+// console.log(firstTwoEls);
+
+// 16
+// function makeArray(firstArray, secondArray, maxLength) {
+//     // Пиши код ниже этой строки
+    
+//    let newArray = firstArray.concat(secondArray);
+
+//     if (newArray.length > maxLength) {
+        
+//          return newArray = newArray.slice(0,maxLength);
+
+//     }
+
+//     return newArray;
+    
+//     // Пиши код выше этой строки
+// }
+
+// console.log(makeArray(['Манго'], ['Аякс', 'Челси', 'Поли', 'Хьюстон'], 3));
+
+// 18
+// function calculateTotal(number) {
+//     // Пиши код ниже этой строки
+//     let total = 0;
+//     for (let i = 1; i <= number; i += 1) {
+//         total+=i;
+//     }
+
+//     return total;
+    
+// }
+//   // Пиши код выше этой строки
+    
+
+//     console.log(calculateTotal(3));
+
+// 19
+// const fruits = ['яблоко', 'слива', 'груша', 'апельсин'];
+
+// for (let i = 0; i<=fruits.length; i+=1) { // Дополни эту строку
+//   const fruit = fruits[i]; // Дополни эту строку
+//   console.log(fruit);
+// }
+
+// 20
+// function calculateTotalPrice(order) {
+//   let total = 0;
+//   // Пиши код ниже этой строки
+// for (let i=0; i<order.length; i+=1){
+//     total += order[i];
+//     console.log(order[i]);
+// }
+//   // Пиши код выше этой строки
+//   return total;
+// }
+// console.log(calculateTotalPrice([164, 48, 291]));
+
+// 21
+function findLongestWord(string) {
+  // Пиши код ниже этой строки
+  
+  let longestWord = string[0];
+    for (let i = 1; i < string.length; i += 1) {
+        if (string[i].length > longestWord.length) {
+            longestWord = string[i];
+        }
+        
+    }
+    return longestWord;
+    } 
+  // Пиши код выше этой строки
+console.log(findLongestWord('Google do a roll'));
