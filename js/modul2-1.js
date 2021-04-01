733e689aa566afaee533ba5621c0198c68c37a19
OlenaTsapliuk <74963920+OlenaTsapliuk@users.noreply.github.com>
2021-04-01 13:52:48 +0300

    all done

--- js/modul2-1.js
+++ js/modul2-1.js
@@ -276,6 +276,9 @@
     
 //     // Пиши код выше этой строки
 // } console.log(getEvenNumbers(3, 11));
+
+
+
 // 32
 function includes(array, value) {
   // Пиши код ниже этой строки
@@ -283,10 +286,13 @@ function includes(array, value) {
   for (let i = 0; i <= array.length; i += 1) {
     if (array[i] === value) {
       return true;
-    } 
+    }
+  }
+      
      return false;
+    
   
     // Пиши код выше этой строки
   }
-}
+
 console.log(includes([1, 2, 3, 4, 5], 2));
